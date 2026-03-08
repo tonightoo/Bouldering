f2fa8870c498f3cac3d56a7676c39247a874bbf5
chocochocolated <tonightoo.development@gmail.com>
2026-03-08 15:13:37 +0900

    add menu

--- main.gd
+++ main.gd
@@ -1,31 +1,52 @@
 extends CanvasLayer
 
 @onready var fade_animation = $FadeLayer
+@onready var title_log = $TitleLog
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$MenuContainer/StartButton.grab_focus()
-	fade_animation.connect_finished(on_fade_animation_finished)
+	animate_logo()
 
+func animate_logo() -> void:
+	var final_pos = Vector2(256.0, 110.0)
+	var start_pos = Vector2(256.0, -200.0)
+	var tween = create_tween()
+	tween.tween_property(title_log, "position", final_pos, 3.5)\
+		.set_trans(Tween.TRANS_BOUNCE)\
+		.set_ease(Tween.EASE_OUT)
 
-func _on_start_button_pressed() -> void:
-	fade_animation.play()
-
-func on_fade_animation_finished(anim_name: StringName) -> void:
-	next_scene()
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		fade_animation.play()
 	
-func next_scene() -> void:
+	if event.is_action("ui_select"):
+		fade_animation.play()
+
+
+func to_tutorial_scene(anim_name: StringName) -> void:
 	get_tree().change_scene_to_file("res://arm_tutorial_stage.tscn")	
 
+func to_story_scene(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://stage_1.tscn")
+	
+
+func _on_start_button_pressed() -> void:
+	fade_animation.connect_finished(to_story_scene)
+	fade_animation.play()
+
+func _on_tutorial_button_pressed() -> void:
+	fade_animation.connect_finished(to_tutorial_scene)
+	fade_animation.play()
+
 func _on_exit_button_pressed() -> void:
-	get_tree().quit()
-
-
-
+	get_tree().quit()	
 
 func _on_start_button_mouse_entered() -> void:
 	$MenuContainer/StartButton.grab_focus()
 
+func _on_tutorial_button_mouse_entered() -> void:
+	$MenuContainer/TutorialButton.grab_focus()
 
 func _on_exit_button_mouse_entered() -> void:
 	$MenuContainer/ExitButton.grab_focus()
